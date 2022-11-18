130331ce2c25d12c29411b0761c74affbf401785
Bourdelas Pablo <pablo.bourdelas@gmail.com>
2022-11-18 16:45:54 +0100

    fix: use npx --no
    npm bin was removed in npm 9

--- packages/cast/smart-contract/tezos/scripts/toolchain/constant.ts
+++ packages/cast/smart-contract/tezos/scripts/toolchain/constant.ts
@@ -1,4 +1,4 @@
 export const CONFIG_FILE = 'smpconfig.json';
-export const SMPC = 'npx smp compile';
+export const SMPC = 'npx --no smp compile';
 export const SMP_TARGET_OBJECT = 'contract';
 export const KEY_GROUP_REGEXP = /tz1@(?<key>[^@]*)@/;
